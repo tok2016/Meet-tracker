c1749aa887bb5bbdad18996f4d3998e934f20858
tvsmsa <tmmelekhin@gmail.com>
2024-10-22 17:59:29 +0500

    Working whisper

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,11 @@
 from fastapi import Depends, FastAPI, HTTPException, Query
-from .routers import users
+from .routers import users, records
 from app.db import create_db_and_tables
 
 app = FastAPI()
 
 app.include_router(users.router, tags=["user"])
+app.include_router(records.router, tags=["record"])
 
 @app.on_event("startup")
 def on_startup():
