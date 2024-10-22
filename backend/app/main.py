21342b0dd6d8fa122a84606e26ced0bdba41e1f0
tvsmsa <tmmelekhin@gmail.com>
2024-10-22 23:13:05 +0500

    Revert "Working whisper"
    This reverts commit c1749aa887bb5bbdad18996f4d3998e934f20858.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,10 @@
 from fastapi import Depends, FastAPI, HTTPException, Query
-from .routers import users, records
+from .routers import users
 from app.db import create_db_and_tables
 
 app = FastAPI()
 
 app.include_router(users.router, tags=["user"])
-app.include_router(records.router, tags=["record"])
 
 @app.on_event("startup")
 def on_startup():
--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,5 +1,5 @@
 from passlib.context import CryptContext
-from typing import Any, Annotated, BinaryIO, Optional, Union
+from typing import Any, Annotated
 import secrets
 import jwt
 from datetime import datetime, timedelta, timezone
@@ -10,10 +10,7 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
 from jwt.exceptions import InvalidTokenError
 from pydantic import ValidationError
-import ffmpeg
-import numpy as np
 
-SAMPLE_RATE = 16000
 
 ALGORITHM = "HS256"
 SECRET_KEY = secrets.token_urlsafe(32)
@@ -60,20 +57,3 @@ def get_current_user(session: SessionDep, token: TokenDep) -> User:
     return user
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
-
-def load_audio(file: BinaryIO, encode=True, sr: int = SAMPLE_RATE):
-    if encode:
-        try:
-            # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
-            # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
-            out, _ = (
-                ffmpeg.input("pipe:", threads=0)
-                .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
-                .run(cmd="ffmpeg", capture_stdout=True, capture_stderr=True, input=file.read())
-            )
-        except ffmpeg.Error as e:
-            raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
-    else:
-        out = file.read()
-
-    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
