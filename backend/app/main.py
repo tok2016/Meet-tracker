e520269c4e22cd191eba3a1c06d7698c1d7067c7
tvsmsa <tmmelekhin@gmail.com>
2024-10-23 16:35:52 +0500

    Cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,24 @@
 from fastapi import Depends, FastAPI, HTTPException, Query
 from .routers import users, records
 from app.db import create_db_and_tables
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router, tags=["user"])
 app.include_router(records.router, tags=["record"])
